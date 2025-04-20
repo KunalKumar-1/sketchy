7a9d4974664d29d4c49bbd4d2c9f3d2ae1d53426
KUNAL KUMAR <kunaldevspro@gmail.com>
2025-04-20 20:11:38 +0530

    (ws): made sure decoded returns as payload object

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -17,6 +17,11 @@ wss.on("connection", function connection(ws, request) {
   const token = queryParams.get("token") || "";
   const decoded = jwt.verify(token, JWT_SECRET) as { userId: JwtPayload };
 
+  if(typeof decoded === "string") {
+    ws.close();
+    return;
+  }
+
   if (!decoded || !decoded.userId) {
     ws.close();
     return;
