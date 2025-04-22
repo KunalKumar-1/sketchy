9b65cdc0e48218edf4c0efa23bfdb46a7f4bf9c9
KUNAL KUMAR <kunaldevspro@gmail.com>
2025-04-22 11:03:30 +0530

    http: linked the @repo/common in http-backend & used the zod schema

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,18 +2,35 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/shared-backend/config";
 import { middleware } from "./middleware";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
 
 const app = express();
 
 app.post("/signup", (req, res) => {
-    // db call
     
+    const parseData = CreateUserSchema.safeParse(req.body);
+    if(!parseData.success) {
+        res.status(400).json({
+            message: "Invalid data"
+        })
+        return;
+    }
+
+    //db call
     res.json({
         userId: "123"
     })
-    })
+})
 
 app.post("/signin", (req, res) => {
+
+    const parseData = SigninSchema.safeParse(req.body);
+    if(!parseData.success) {
+        res.status(400).json({
+            message: "Invalid inputs"
+        })
+        return;
+    }
 
     const userId = 1;
     const token = jwt.sign({
